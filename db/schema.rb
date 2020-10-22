1b0b701f0ae04f71d7465da5b735418d19b1c8ca
Lauren Anderson <laurenaanderson95@gmail.com>
2020-10-21 20:14:19 -0400

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20201017191213) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
   end
