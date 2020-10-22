788f2070768e7bb61d7089cf96d0b8c686a3e74e
Lauren Anderson <laurenaanderson95@gmail.com>
2020-10-22 19:38:44 -0400

    added session

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,14 +9,16 @@ class PlantsController < ApplicationController
        erb :"plants/new"
     end
 
-    get '/:id' do
+    get '/plants/:id' do
         @plants = Plant.find(params[:id])
         erb :"plants/show"
     end
 
     post '/plants' do
         @plants = Plant.new
-        
+        @plants.name = params[:name]
+        @plants.description = params[:description]
+        redirect "/plants/#{plants.id}"
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,24 @@ class UsersController < ApplicationController
         erb :'/users/signup'
     end
 
+    post '/users' do
+        @users = User.create(params)
+        if @users.valid?
+            session[:user_id] = @users.id
+            redirect "/users/#{@users.username}"
+        else
+            redirect '/signup'
+        end
+    end
+
+    get '/users/:username' do
+        
+    end
+
     get '/login' do
         erb :'/users/login'
     end
+
+
 
 end
