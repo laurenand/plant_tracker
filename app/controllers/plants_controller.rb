71702ccb91a8a7e2c7c5b3aba79ddddd9914c8a8
Lauren Anderson <laurenaanderson95@gmail.com>
2020-10-24 13:10:54 -0400

    added users controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,19 +5,27 @@ class PlantsController < ApplicationController
         erb :"/plants/index"
     end
 
+    #allows you to add a new plant
     get '/new' do 
-       erb :"plants/new"
+        if logged_in?
+            erb :"plants/new"
+        else 
+            redirect_if_not_logged_in
+        end
     end
 
+    #shows plants
     get '/plants/:id' do
         @plants = Plant.find(params[:id])
         erb :"plants/show"
     end
 
+    #creates plants
     post '/plants' do
         @plants = Plant.new
         @plants.name = params[:name]
         @plants.description = params[:description]
+        @plants.save
         redirect "/plants/#{plants.id}"
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,34 @@ class UsersController < ApplicationController
     end
 
     get '/users/:username' do
-        
+        @users = User.fin_by(params)
+        if @users == nil
+            redirect '/'
+        end
+        erb :"users/show"
     end
 
     get '/login' do
         erb :'/users/login'
     end
 
+    post '/login' do
+        @users = User.find_by(username: params[:username])
+        if @users && @users.authenticate(params[:password])
+            session[:user_id] = @users.id
+            redirect "users/#{@users.username}"
+        else
+            redirect '/error'
+        end
+    end
 
+    get '/error' do
+        erb :"users/error"
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
 
 end
